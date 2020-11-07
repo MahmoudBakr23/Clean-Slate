1217a341804dd0f5fab2d66fedd458c8167657e2
Mahmoud Bakr <mbakr6821@gmail.com>
2020-11-07 15:50:01 +0200

    setup: navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+	include SessionsHelper
+	
+	def navigation
+		out = []
+		if current_user
+			out << link_to(current_user.name, user_path(current_user), class: "nav-link text-secondary")
+			out << link_to('Logout', logout_path, method: :delete, class: "nav-link text-secondary")
+			out << link_to('Delete Account', user_path(current_user), method: :delete, data: { confirm: 'Are you sure?' }, class: "nav-link text-danger")
+		else
+			out << link_to('Login', login_path, class: 'nav-link text-secondary')
+      out << link_to('Sign up', sign_up_path, class: 'nav-link text-secondary')
+		end
+		out.join("|").html_safe
+	end
 end
